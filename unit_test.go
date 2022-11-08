e89c97e9760d6cbb188f6624401d32ce36771ed5
Sourabh Kumar Sahu <40160064+sourabh179@users.noreply.github.com>
2022-11-09 00:22:15 +0530

    Updated unit test cases for different scenarios
    Updated unit test cases for different scenarios

--- unit_test.go
+++ unit_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+//Testcase to test the status code for GET Method
 func TestGetEntries(t *testing.T) {
 	req, err := http.NewRequest("GET", "/statistics", nil)
 	if err != nil {
@@ -48,4 +50,101 @@ func TestGetEntries(t *testing.T) {
 			rr.Body.String(), expected)
 
 	}*/
+}
+
+//Test Case to test transaction
+func TestCreateTransaction(t *testing.T) {
+
+	var jsonStr = []byte(`{"amount":"100.25","timestamp":"2021-07-17T09:59:51.312Z"}`)
+
+	req, err := http.NewRequest("POST", "/transaction", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Transactions)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+}
+
+//Testcase to test statistic after transaction
+func TestGetStatistics(t *testing.T) {
+	req, err := http.NewRequest("GET", "/statistics", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Statistics)
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "{\"avg\":\"100.250000\",\"count\":\"1\",\"max\":\"100.250000\",\"min\":\"100.250000\",\"sum\":\"100.250000\"}"
+	//expected := "{\"avg\":\"0.000000\",\"count\":\"0\",\"max\":\"0.000000\",\"min\":\"0.000000\",\"sum\":\"0.000000\"}"
+	out := rr.Body.String()
+	if out == expected {
+		t.Logf("Expected to get status %s is same ast %s\n",rr.Body.String(), expected)
+
+	}else {
+		t.Errorf("handler returned unexpected body: got %v want %v ",
+			rr.Body.String(), expected)
+
+	}
+}
+
+//Testcase to delete statistic
+func TestDeleteEntries(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/delete", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Delete)
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusNoContent {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNoContent)
+	}
+}
+
+//Testcase to test invalid json
+func TestInvalidJson(t *testing.T) {
+	var jsonStr = []byte(`{"amount":100.25","timestamp":"2021-07-17T09:59:51.312Z"}`)
+	req, err := http.NewRequest("POST", "/transaction", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Transactions)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusUnprocessableEntity {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnprocessableEntity)
+	}
+}
+
+//To test 400 bad request
+func TestBadRequest(t *testing.T) {
+	//var jsonStr = []byte(`{"amount":100.25","timestamp":"2021-07-17T09:59:51.312Z"}`)
+	req, err := http.NewRequest("POST", "/transaction", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Transactions)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
 }
